2cf9a176722dbeee13d468ed0e38b92b9dfa41bd
radekska <r.skalbania@piwik.pro>
2023-10-28 18:07:04 +0200

    wip

--- main.go
+++ main.go
@@ -9,6 +9,68 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const (
+	usersTable     = "users"
+	usersDataTable = "userdata"
+)
+
+type User struct {
+	ID          int
+	Username    string
+	Name        string
+	Surname     string
+	Description string
+}
+// TODO: add update, delete and fix list all
+
+func userExists(db *sql.DB, username string) (string, error) {
+	var id string
+	err := db.QueryRow(fmt.Sprintf("SELECT id FROM %s WHERE username = $1", usersTable), username).Scan(&id)
+	if err != nil {
+		return "", err
+	}
+	return id, nil
+}
+
+func AddUser(db *sql.DB, user User) error {
+	_, err := db.Exec(fmt.Sprintf("INSERT INTO %s (username) VALUES ($1)", usersTable), user.Username)
+	if err != nil {
+		return err
+	}
+
+	userID, err := userExists(db, user.Username)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(
+		fmt.Sprintf("INSERT INTO %s (userid, name, surname, description) VALUES ($1, $2, $3, $4)",
+			usersDataTable),
+		userID,
+		user.Name,
+		user.Surname,
+		user.Description,
+	)
+	return err
+}
+
+func getUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM %s", usersTable))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var users []User
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.ID, &user.Username, &user.Name, &user.Surname, &user.Description)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
+
 func main() {
 	args := os.Args
 	if len(args) != 6 {
@@ -67,4 +129,17 @@ func main() {
 		fmt.Println("+T", name)
 	}
 	defer rows.Close()
+
+	err = AddUser(db, User{Username: "john123", Name: "John", Surname: "Doe", Description: "A user"})
+	if err != nil {
+		fmt.Println("Error adding user", err)
+		return
+	}
+
+	// users, err := getUsers(db)
+	// if err != nil {
+	// 	fmt.Println("Error getting users", err)
+	// 	return
+	// }
+	// fmt.Println(users)
 }
